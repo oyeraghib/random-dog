8fd9b22bfb2e38ec8c2c2fe8e74a848133e4f5e0
oyeraghib <raghib.khan76@gmail.com>
2022-11-28 20:45:30 +0530

    update main application to have single instance of repo

--- app/src/main/java/com/example/randomdoggen/MainApplication.kt
+++ app/src/main/java/com/example/randomdoggen/MainApplication.kt
@@ -1,13 +1,22 @@
 package com.example.randomdoggen
 
 import android.app.Application
+import com.example.randomdoggen.api.client.RandomDogClient
+import com.example.randomdoggen.app.repository.RandomDogRepo
 import timber.log.Timber
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        //Timber
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
     }
+    //Single Repo Instance
+    /**
+     *This makes sure that there is single instance of repository throughout the whole application.
+     */
+    val randomDogRepo by lazy { RandomDogRepo(RandomDogClient) }
 }
