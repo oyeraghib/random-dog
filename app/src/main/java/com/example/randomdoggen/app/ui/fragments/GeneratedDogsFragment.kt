c36819921b28f320174c1a801203fb54ef2bec7b
oyeraghib <raghib.khan76@gmail.com>
2022-11-28 18:06:41 +0530

    add new fragments and update manifest

---
+++ app/src/main/java/com/example/randomdoggen/app/ui/fragments/GeneratedDogsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.randomdoggen.app.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.randomdoggen.R
+
+class GeneratedDogsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_generated_dogs, container, false)
+    }
+}
