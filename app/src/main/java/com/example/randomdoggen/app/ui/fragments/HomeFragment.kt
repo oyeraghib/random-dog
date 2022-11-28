00300a2eaf080466c2c0936d64693876a7309538
oyeraghib <raghib.khan76@gmail.com>
2022-11-28 18:50:26 +0530

    NF: update Home Fragment to inflate views using view binding

--- app/src/main/java/com/example/randomdoggen/app/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/randomdoggen/app/ui/fragments/HomeFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.randomdoggen.R
+import com.example.randomdoggen.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
