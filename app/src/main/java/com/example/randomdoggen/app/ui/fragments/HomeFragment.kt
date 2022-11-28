6e2c0f9c7489ce94a292de0bf003b161485671fa
oyeraghib <raghib.khan76@gmail.com>
2022-11-28 19:04:28 +0530

    fix: background color of buttons
    remove the card view causing unnecessary shadow to the button and update the fragment classes

--- app/src/main/java/com/example/randomdoggen/app/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/randomdoggen/app/ui/fragments/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.randomdoggen.R
 import com.example.randomdoggen.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -19,6 +21,20 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            tvGenerateDog.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_generateDogFragment)
+            }
+
+            tvGeneratedDogs.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_generatedDogsFragment)
+            }
+        }
     }
 
     override fun onDestroyView() {
