01383a3d727033573e5299ce88fdda4fbf97fa65
oyeraghib <raghib.khan76@gmail.com>
2022-11-28 20:46:13 +0530

    update fragment to load images

--- app/src/main/java/com/example/randomdoggen/app/ui/fragments/GenerateDogFragment.kt
+++ app/src/main/java/com/example/randomdoggen/app/ui/fragments/GenerateDogFragment.kt
@@ -5,15 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.randomdoggen.R
+import androidx.fragment.app.viewModels
+import coil.load
+import com.example.randomdoggen.MainApplication
+import com.example.randomdoggen.app.viewmodel.ViewModelFactory
+import com.example.randomdoggen.app.viewmodel.RandomDogViewModel
+import com.example.randomdoggen.databinding.FragmentGenerateDogBinding
 
 class GenerateDogFragment : Fragment() {
+
+    //ViewModel
+    private val randomDogViewModel: RandomDogViewModel by viewModels {
+        ViewModelFactory(
+            (context?.applicationContext as MainApplication).randomDogRepo
+        )
+    }
+
+    private var _binding: FragmentGenerateDogBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generate_dog, container, false)
+        _binding = FragmentGenerateDogBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            tvGenerate.setOnClickListener {
+                randomDogViewModel.generateRandomDog()
+                randomDogViewModel.randomDog.observe(viewLifecycleOwner) {
+                    //TODO: Add progress bar while image is being loaded.
+                    ivImage.load(it.message)
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
