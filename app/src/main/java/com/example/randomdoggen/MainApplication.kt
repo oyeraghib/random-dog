b267d3205b3dbf9296b75c9a5fcbd7f27601f8d2
oyeraghib <raghib.khan76@gmail.com>
2022-11-29 20:43:27 +0530

    update repo instance in MainApplication.kt

--- app/src/main/java/com/example/randomdoggen/MainApplication.kt
+++ app/src/main/java/com/example/randomdoggen/MainApplication.kt
@@ -2,6 +2,7 @@ package com.example.randomdoggen
 
 import android.app.Application
 import com.example.randomdoggen.api.client.RandomDogClient
+import com.example.randomdoggen.app.database.cacheDatabase.DogsDatabase
 import com.example.randomdoggen.app.repository.RandomDogRepo
 import timber.log.Timber
 
@@ -14,9 +15,15 @@ class MainApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
     }
+    //Single global database instance
+    /**
+     * This helps to have single instance of database throughout the whole app lifecycle
+     */
+    private val database by lazy { DogsDatabase.getDatabase(this) }
+
     //Single Repo Instance
     /**
      *This makes sure that there is single instance of repository throughout the whole application.
      */
-    val randomDogRepo by lazy { RandomDogRepo(RandomDogClient) }
+    val randomDogRepo by lazy { RandomDogRepo(RandomDogClient, database.dogsDao()) }
 }
