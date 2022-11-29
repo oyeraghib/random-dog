524704ef70991e3d5d05ddb5747cde26fdea628d
oyeraghib <raghib.khan76@gmail.com>
2022-11-29 20:42:30 +0530

    add dao

---
+++ app/src/main/java/com/example/randomdoggen/app/database/dao/DogsDao.kt
@@ -0,0 +1,24 @@
+package com.example.randomdoggen.app.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.randomdoggen.app.database.entity.DogsEntity
+
+@Dao
+interface DogsDao {
+
+    //Cache dog
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveDog(url: DogsEntity)
+
+    //Read list of dogs
+    @Query("select image_url from dogs_entity ORDER BY id desc")
+    fun readDogs(): LiveData<List<String>>
+
+    //Delete all dogs
+/*    @Query("delete from dogs_entity")
+    suspend fun deleteAllDogs()*/
+}
