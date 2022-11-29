a46c9460f74e384ba98b1b1962e15f8c6a725eb2
oyeraghib <raghib.khan76@gmail.com>
2022-11-29 20:43:10 +0530

    update viewmodel with new dao

--- app/src/main/java/com/example/randomdoggen/app/viewmodel/RandomDogViewModel.kt
+++ app/src/main/java/com/example/randomdoggen/app/viewmodel/RandomDogViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.randomdoggen.api.models.response.GenerateDogResponse
+import com.example.randomdoggen.app.database.entity.DogsEntity
 import com.example.randomdoggen.app.repository.RandomDogRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,7 +18,21 @@ class RandomDogViewModel(private val repository: RandomDogRepo) : ViewModel() {
 
     fun generateRandomDog() = viewModelScope.launch(Dispatchers.IO) {
         repository.generateRandomDog().let {
+            saveDog(DogsEntity(imageUrl = "${it.message}"))
             _randomDog.postValue(it)
         }
     }
+
+    //Save dog image
+    private fun saveDog(image: DogsEntity) = viewModelScope.launch(Dispatchers.IO) {
+        repository.saveDog(image)
+    }
+
+    //Read all dogs
+    val readAllDogs: LiveData<List<String>> = repository.readAllDogs()
+
+  /*  //Delete all dogs
+    val deleteAllDogs = viewModelScope.launch(Dispatchers.IO) {
+        repository.deleteDogs()
+    }*/
 }
