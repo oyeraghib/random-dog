f38052c1a16ec474697aee88def2ca1811965ef4
oyeraghib <raghib.khan76@gmail.com>
2022-11-29 16:23:12 +0530

    feat: add adapter to recycler view

--- app/src/main/java/com/example/randomdoggen/app/ui/fragments/GeneratedDogsFragment.kt
+++ app/src/main/java/com/example/randomdoggen/app/ui/fragments/GeneratedDogsFragment.kt
@@ -5,15 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.randomdoggen.R
+import com.example.randomdoggen.app.adapter.GeneratedDogsAdapter
+import com.example.randomdoggen.databinding.FragmentGeneratedDogsBinding
 
 class GeneratedDogsFragment : Fragment() {
+
+    private var _binding: FragmentGeneratedDogsBinding? = null
+    private val binding get() = _binding!!
+
+    //Adapter
+    private lateinit var myAdapter: GeneratedDogsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generated_dogs, container, false)
+        _binding = FragmentGeneratedDogsBinding.inflate(layoutInflater)
+        myAdapter = GeneratedDogsAdapter()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvDogsCachedList.adapter = myAdapter
+
+        //Retrieving
+        val list = mutableListOf<String>()
+        list.add("https://images.dog.ceo/breeds/affenpinscher/n02110627_3286.jpg")
+        list.add("https://images.dog.ceo/breeds/mastiff-english/2.jpg")
+        myAdapter.getDogs(list)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
